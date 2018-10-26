3086b201f5aba68d054d21f1d7755f60e77c414b
Marco António Alemão <marco_alemao@hotmail.com>
2018-10-26 14:17:28 +0200

    Delete main.cpp

--- INEX/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "ros/ros.h"
-#include <iostream>
-
-int main(int argc, char *argv[]) {
-  ros::init(argc, argv, "INEX");
-	std::cout << "Hello new world, lets find extraterrestrials!!!";
-
-return 900;
-}
