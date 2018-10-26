dda92a79a462fad7f7fbe703b0a7663b2ee612ec
Marco António Alemão <marco_alemao@hotmail.com>
2018-10-26 14:06:22 +0200

    Add files via upload

---
+++ INEX/src/main.cpp
@@ -0,0 +1,9 @@
+#include "ros/ros.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "INEX");
+	std::cout << "Hello new world, lets find extraterrestrials!!!";
+
+return 900;
+}
