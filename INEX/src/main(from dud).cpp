69bdcc5211af7d4a68e68565156a1954c9d23acb
Marco António Alemão <marco_alemao@hotmail.com>
2018-11-28 22:39:36 +0100

    Update main(from dud).cpp

--- INEX/src/main(from dud).cpp
+++ INEX/src/main(from dud).cpp
@@ -6,34 +6,9 @@
 #include <fstream>
 #include <std_msgs/Header.h>
 
-
-void test_save_my_map() {
-  // create some fake map data
-    std::ifstream myfile ("/home/dud/Desktop/myMap.pmg");
-    int8_t image[] = ;
-    std::cout << image;
-
-	//nav_msgs::OccupancyGrid* newGrid = map.Grid();
-	//newGrid->header = header;
-	
-    myfile << image;
-    myfile.close();
-    }
-  fakeData = { header = myFakeHeader, data = myFakeData }
-  // call savemymap with fake data
-  save_my_map(fakeData);
-  // see if data is being saved
-  if(fileContains(myFakeData) {
-	// we did well
-  } else {
-   // we failed..
-  }
-}
-
 void save_my_map(const nav_msgs::OccupancyGrid::ConstPtr& mMap){
    std::ofstream myfile ("/home/dud/ros_ws/src/INEX/map/mars_map.pgm");
-    if (myfile.is_open())
-    {
+    if (myfile.is_open()){
 	int8_t image[] = mMap->data;
 	std::cout << image 
 
@@ -45,35 +20,36 @@ void save_my_map(const nav_msgs::OccupancyGrid::ConstPtr& mMap){
     }
     else std::cout << "Unable to open file";
 }
+
 int main(int argc, char *argv[]) {
-  ros::init(argc, argv, "inex");
-  ros::NodeHandle nh;
-  //get the map and into raw_map
-  nav_msgs::OccupancyGrid raw_map;
-  ros::Subscriber map = nh.subscribe("map", 200, save_my_map); //"nav_msgs/OccupancyGrid", 2, save_my_map);
+	std::cout << "Hello new world, lets find extraterrestrials!!!";
+  	ros::init(argc, argv, "inex");
+  	ros::NodeHandle nh;
+  	//get the map and into raw_map
+  	nav_msgs::OccupancyGrid raw_map;
+  	ros::Subscriber map = nh.subscribe("map", 20, save_my_map); //"nav_msgs/OccupancyGrid", 2, save_my_map);
 
-  /*
-  ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 10);
+	/*ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 10);
 
-  //get the map and into raw_map
-  nav_msgs::OccupancyGrid raw_map;
-  ros::Subscriber map = nh.subscribe("nav_msgs/OccupancyGrid", 2, save_my_map);
+	//get the map and into raw_map
+	nav_msgs::OccupancyGrid raw_map;
+	ros::Subscriber map = nh.subscribe("nav_msgs/OccupancyGrid", 2, save_my_map);
 
-  ros::Duration(1.0).sleep();
+	ros::Duration(1.0).sleep();
 
-  /*geometry_msgs::Twist msg;
-  msg.linear.x = 1.0;
-  cmd_vel_pub.publish(msg);
+	geometry_msgs::Twist msg;
+	msg.linear.x = 1.0;
+	cmd_vel_pub.publish(msg);
 
-  ros::Duration(1.0).sleep();
+	ros::Duration(1.0).sleep();
 
-  msg.linear.x = 0.0;
-  msg.angular.z = 1.0;
-  cmd_vel_pub.publish(msg);
+	msg.linear.x = 0.0;
+	msg.angular.z = 1.0;
+	cmd_vel_pub.publish(msg);
 
-  ros::Duration(1.0).sleep();*/
-  ros::spin();
+	ros::Duration(1.0).sleep();*/
+	ros::spin();
 
-  return 0;
+  	return 0;
 }
 
