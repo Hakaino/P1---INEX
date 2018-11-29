9ddbe83417dea09be322e54e9f4fda8a3fd013d6
Marco António Alemão <marco_alemao@hotmail.com>
2018-11-29 20:36:31 +0100

    Update main.cpp

--- INEX/src/main.cpp
+++ INEX/src/main.cpp
@@ -7,6 +7,9 @@
 #include <std_msgs/Header.h>
 
 void save_my_map(const nav_msgs::OccupancyGrid::ConstPtr& mMap){
+	/*This recieves an OccupancyGrid object and generate a map,
+	it updates everytime that there are changes.
+	it draws the file pixel by pixel.*/
 	std::string  mapName = "/home/dud/ros_ws/src/INEX/map/mars_map.pgm";
 	ROS_INFO("\nNow I will draw the map");
 	FILE* mapFile = fopen(mapName.c_str(), "w");
@@ -35,29 +38,7 @@ void save_my_map(const nav_msgs::OccupancyGrid::ConstPtr& mMap){
 int main(int argc, char *argv[]){
 	ros::init(argc, argv, "inex");
 	ros::NodeHandle nh;
-	//this subscribes to the topic map from map_server
 	ros::Subscriber map = nh.subscribe("map", 20, save_my_map);
-
-  /*
-  ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 10);
-
-  //get the map and into raw_map
-  nav_msgs::OccupancyGrid raw_map;
-  ros::Subscriber map = nh.subscribe("nav_msgs/OccupancyGrid", 2, save_my_map);
-
-  ros::Duration(1.0).sleep();
-
-  /*geometry_msgs::Twist msg;
-  msg.linear.x = 1.0;
-  cmd_vel_pub.publish(msg);
-
-  ros::Duration(1.0).sleep();
-
-  msg.linear.x = 0.0;
-  msg.angular.z = 1.0;
-  cmd_vel_pub.publish(msg);
-
-  ros::Duration(1.0).sleep();*/
   ros::spin();
 
   return 0;
