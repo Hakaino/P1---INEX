00083cc2f43b346a224d2be7eda193d832424141
Marco António Alemão <marco_alemao@hotmail.com>
2018-12-04 08:58:36 +0100

    Update pathplaner.h

--- INEX/src/pathplaner.h
+++ INEX/src/pathplaner.h
@@ -1,54 +1,71 @@
+
 #include <iostream>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <linux/input.h>
-#include <iostream>
-#include <map>
-#include <cmath>
 #include <math.h>
-#include <queue>
 #include <vector>
 
 int distance = 10;
-int complexity = 4; //the bigger the number, the roundest will be the path
+int complexity = 4;
 
-std::vector<int[2]> getAdjacent(int lastPoint[2]){ //, std::vector<int[2]> missedPoints){
-    std::vector<int[2]> adjacent;
+std::vector<int> getAdjacent(int x, int y){ //, std::vector<int> missedPoints){
+    int n, a, b;
+    std::vector<int> adjacent, holder;
+    double angle;
     //Characterize the current position
-    int L = sqrt(pow(lastPoint[0], 2) + pow(lastPoint[1], 2)) / distance;
-    float teta = acos(lastPoint[0] / distance);
-    //int N = pow(L, 2) * complexity;
-    //float delta = 2 * M_PI / N;
-    //float alpha = teta + delta;
-    //float beta = teta - delta;
-    //TODO: get the points of lower level inside the angle
-    int n = static_cast<int>(pow(L - 1, 2) * complexity);
-    int a = static_cast<int>(n * teta / (2 * M_PI) - pow(1 - 1 / L, 2));
-    int b = static_cast<int>(n * teta / (2 * M_PI) + pow(1 - 1 / L, 2));
-    for (int i = a; i <= b; i++){
-        double angle = i * 2 * M_PI / n;
-        int pt[] = {int(L * distance * cos(angle)), int(L * distance * sin(angle))};
-        adjacent.push_back(pt);
+    int l = sqrt(pow(x, 2) + pow(y, 2)) / distance;
+    int L = l;
+    int N = L * complexity;
+    double teta;
+    if (x == 0 && y > 0) teta = M_PI; else if (x == 0 && y > 0) teta = M_PI * 3 / 2; else teta = atan(y / x);
+    double delta = 2 * M_PI / N;
+    //Get the points from the lower level inside the angle
+    n = (L - 1) * complexity;
+    for (int i = 0; i < n; i++){
+        angle = i * 2 * M_PI / n;
+        if (angle <= teta + delta && angle >= teta - delta){
+            holder.push_back(int((L - 1) * distance * cos(angle)));
+            holder.push_back(int((L - 1) * distance * sin(angle)));
+            std::cout << "#";
+        }
     }
-    //TODO: get the 2 adjacent points in the same level
-    //TODO: get the next level points in angle
-    return adjacent;
+    //get the 2 adjacent points in the same level
+    holder.push_back(int(L * distance * cos(teta + delta)));
+    holder.push_back(int(L * distance * sin(teta + delta)));
+    holder.push_back(int(L * distance * cos(teta - delta)));
+    holder.push_back(int(L * distance * sin(teta - delta)));
+    //get the upper level points in angle
+    n = (L + 1) * complexity;
+    for (int i = 0; i < n; i++){
+        angle = i * 2 * M_PI / n;
+        if (angle <= teta + delta && angle >= teta - delta){
+            holder.push_back(int((L + 1) * distance * cos(angle)));
+            holder.push_back(int((L + 1) * distance * sin(angle)));
+            std::cout << "*";
+        }
+    }
+    //erase repeated points
+    adjacent.push_back(holder[0]);
+    adjacent.push_back(holder[1]);
+    for (int i = 2; i < holder.size(); i += 2)
+        if (holder[i] != holder[i - 2] && holder[i + 1] != holder[i - 1]){
+            adjacent.push_back(holder[i]);
+            adjacent.push_back(holder[i - 1]);
+        } else std::cout << "not eficient yet, it repeats this point: " << holder[i] << ", " << holder[i + 1] << "\n";
+    return holder;
 }
 
 /*bool goodPoint(int* point[2]){
-    return true;
+    if (not point not in map) return true;
+    else return false;
 }*/
 
-void brain(int* lastPoint[2]){
+void brain(int* x, int* y){
     bool clockwise;
-    //Finds the next point
-
-    //continues from here
-    std::vector<int[2]> adjacent = getAdjacent(*lastPoint); //, missed); //order: anticlockwize lower level, anticlockwi level, anticlockwise level, next level
-    //TODO: case > has adjacent lower level unmapped point
-    for (std::vector<int[2]>::iterator point = adjacent.begin() ; point != adjacent.end(); point++) {
-        if (true){ //goodPoint(*point)) {
+    //Finds the next point and sort them in order or priority
+    //clockwise, lower level, current level, upper level
+    std::vector<int> adjacent = getAdjacent(*x, *y);
+    //TODO: check in order if a point can be a goal
+    for (std::vector<int>::iterator point = adjacent.begin() ; point != adjacent.end(); point += 2) {
+        if (true){ //goodPoint(*point, point + 1)) { //may have error
             break;
         } else {
             //expand search in the radios of the current point
@@ -63,5 +80,9 @@ void brain(int* lastPoint[2]){
 }
 
 int main (){
+    //just testing
+    std::vector<int> points = getAdjacent(2000, 20000);
+    for (int i = 0; i < points.size(); i += 2)
+        std::cout << points[i] << ", " << points[i + 1] << "\n";
     return 0;
 }
