e8eac994e0e6b91cfe1e353d973242037bea48a4
Marco António Alemão <marco_alemao@hotmail.com>
2018-12-18 17:34:12 +0100

    Create get.sh

---
+++ remote/src/get.sh
@@ -0,0 +1,10 @@
+#wizard@192.168.0.3:home/wizard/Documents/Livros/Escola/AAU/P1/Development
+#ros@192.168.0.4:home/Desktop/INEX
+#
+
+rsync dud@192.168.0.2:home/dud/ros_ws/src/INEX/photos.tar.gz home/Documents/Livros/Escola/AAU/P1/Development/Ros/src/remote
+ros
+rsync dud@192.168.0.2:home/dud/ros_ws/src/INEX/anomalies.tar.gz home/Documents/Livros/Escola/AAU/P1/Development/Ros/src/remote
+ros
+rsync dud@192.168.0.2:home/dud/ros_ws/src/INEX/map/mars_map.pgm home/wizard/Documents/Livros/Escola/AAU/P1/Development/Ros/src/remote
+ros
