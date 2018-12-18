47c80f314ea01537c9afa189d600b8f6fe7f729f
Marco António Alemão <marco_alemao@hotmail.com>
2018-12-18 17:40:38 +0100

    Update inex.cpp

--- INEX/src/inex.cpp
+++ INEX/src/inex.cpp
@@ -1,20 +1,30 @@
 #include "ros/ros.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include <iostream>
-//#include <sensor_msgs/BatteryState>
+#include "std_msgs/Char.h"
+#include "sensor_msgs/BatteryState.h"
 
 
-/*void change_mode(const ... key){
+void executer(const std_msgs::Char::ConstPtr& key){
   //changes betwee manual and autonumous navigation
-  if (key == 'a' || key == 'A') std::system("/home/dud/ros_ws/src/INEX/src/auto.sh");
-  else if (key == 'm' || key == 'M') std::system("/home/dud/ros_ws/src/INEX/src/manual.sh");
-  else if (key == 'm' || key == 'M') std::system("/home/dud/ros_ws/src/INEX/src/transmit.sh");
-}*/
+  if (key->data == 'a' || key->data == 'A') {
+	std::system("/home/dud/ros_ws/src/INEX/src/auto.sh");
+	ROS_INFO("[Autonumous mode activate]");
+  }
+  else if (key->data == 'm' || key->data == 'M') {
+	std::system("/home/dud/ros_ws/src/INEX/src/manual.sh");
+	ROS_INFO("[Manual mode activate]");
+  }
+  else if (key->data == 't' || key->data == 'T') {
+	std::system("/home/dud/ros_ws/src/INEX/src/compress.sh");
+	ROS_INFO("[Compressing data for transmission]");
+  };
+}
 
-/*void low_power(const sensor_msgs::BatteryState::::ConstPtr& power){
+void low_power(const sensor_msgs::BatteryState::ConstPtr& power){
   //if the power is too low, kill all movement
-  if (power->cell_voltage[0] < 10) std::system("/home/dud/src/INEX/src/power.sh");
-}*/
+  if (power->percentage < 0.1) std::system("/home/dud/src/INEX/src/power.sh");
+}
 
 void set_map(const nav_msgs::OccupancyGrid::ConstPtr& mMap){
     //This renders an updated 2D image of the map.
@@ -46,17 +56,8 @@ void set_map(const nav_msgs::OccupancyGrid::ConstPtr& mMap){
 int main(int argc, char *argv[]) {
     ros::init(argc, argv, "inex");
     ros::NodeHandle nh;
-    /*
-    ros::Subscriber keys = nh.subscribe("keys, 5, change_mode);
-    ros::subscriber power = nh.subscribe("power, 5, low_power);
-    ros::Subscriber picture = nh.subscribe("map, 5, set_map);
-    ros::Subscriber power = nh.subscribe("sensor_msgs/BatteryState", 5, low_power);
-    */
+    ros::Subscriber keys = nh.subscribe("command_topic", 5, executor);
     ros::Subscriber map = nh.subscribe("map", 5, set_map);
-    ros::Duration(10.0).sleep();
-    
-//std::system("/home/dud/ros_ws/src/INEX/src/manual.sh");
-
+    ros::Subscriber power = nh.subscribe("sensor_msgs/BatteryState", 5, low_power);
     ros::spin();
 }
-
